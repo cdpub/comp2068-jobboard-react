5027749e72e620b79c15bd3b7b17172ccce057e8
CynthiaPublico <Cynthia.Dejesus-Publico@MyGeorgian.ca>
2019-08-12 16:30:35 -0400

    f

---
+++ client/build/precache-manifest.da84179c0ce5d44e888fab35d65e3ee0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063c84944d056f8635df6727f69542d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e979cc00785b33d7348",
+    "url": "/static/js/2.89711a5c.chunk.js"
+  },
+  {
+    "revision": "f9f138d5a7ffd7b1b554",
+    "url": "/static/js/main.7d75001a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
